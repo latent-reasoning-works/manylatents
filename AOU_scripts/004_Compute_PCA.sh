f9eff6de8908324ae95150a533ec7dd42580eae8
César Miguel Valdez Córdova <cesar.valdez@mila.quebec>
2025-01-06 03:33:27 -0500

    remove AOU folder

--- AOU_scripts/004_Compute_PCA.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 compute_pca.py --plink_file_root Data/1KGPHGDPAOU_V7 \
-                       --plink_file_name extractedChrAllPruned \
-                       --output_folder Data/1KGPHGDPAOU_V7_EMBS \
-                       --pca_components 50 \
-                       --chunk_size 5000 \
-                       --subset '1KGP'
