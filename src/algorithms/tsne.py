b96298e4e0a0fce04a63aa3b83aa42cd5778fda7
nishuang83 <nishuang83@gmail.com>
2025-02-18 22:00:37 -0500

    update tsne module and config

--- src/algorithms/tsne.py
+++ src/algorithms/tsne.py
@@ -15,28 +15,42 @@ class TSNEModule(LightningModule):
         network: Optional[torch.nn.Module] = None, # Unused for now; kept for interface consistency
         random_state: int = 42,
         perplexity: float = 30.0, 
+        n_iter: int = 1000,
+        learning_rate: int = 'auto',
+        metric: str = "euclidean",
         fit_fraction: float = 1.0,  # Fraction of the first batch to use for fitting t-SNE
-        **kwargs
     ):
         super().__init__()
         self.n_components = n_components
         self.datamodule = datamodule
         self.random_state = random_state
         self.perplexity = perplexity
-        self.tsne_params = kwargs # Additional t-SNE parameters
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+        self.metric = metric
         self.fit_fraction = fit_fraction
         self._is_fitted = False
         self.model = TSNE(n_components=self.n_components, 
-                         random_state=self.random_state, 
-                         perplexity=self.perplexity,
-                         **self.tsne_params
-                         ), 
+                          random_state=self.random_state, 
+                          perplexity=self.perplexity,
+                          n_iter=self.n_iter,
+                          learning_rate=self.learning_rate,
+                          metric=self.metric
+                         )
 
-    def forward(self, x: Tensor) -> Tensor:
+    def fit_transform(self, x: Tensor) -> Tensor:
         """
         t-SNE does not support separate fit and transform, this function fit and transform on same fraction.
         """
-        x_np = x.detach().cpu().numpy()
+        if isinstance(x, torch.Tensor):
+            device = x.device
+            dtype = x.dtype
+            x_np = x.detach().cpu().numpy()
+        else:
+            device = torch.device("cpu")
+            dtype = torch.float32
+            x_np = x
+
         if not self._is_fitted:
             n_samples = x_np.shape[0]
             # Use only a fraction of the first batch for fitting.
@@ -44,7 +58,7 @@ class TSNEModule(LightningModule):
             embedding = self.model.fit_transform(x_np[:n_fit])
             self._is_fitted = True
 
-        return torch.tensor(embedding, device=x.device, dtype=x.dtype)
+        return torch.tensor(embedding, device=device, dtype=dtype)
 
     def training_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int):
         return self.shared_step(batch, batch_idx, phase="train")
