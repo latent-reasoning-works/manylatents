878be4fad3f01b34df74a1f8bc40c2a5fa914142
César Miguel Valdez Córdova <cesar.valdez@mila.quebec>
2025-02-19 21:45:15 -0500

    update paths with new dropbox links

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,14 +5,14 @@
 mkdir -p data/HGDP+1KGP/genotypes
 
 # Define the URLs with dl=1 to force a direct download.
-URL_BED="https://www.dropbox.com/scl/fo/3wyjvc06ch4agtvslfrk4/AGpa4DQta19mLwghQ9RxWdM/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.match1000G_GSAs_dietnet.bed?rlkey=bkanr0l2lvbqrd037j7kjeovy&e=1&dl=1"
-URL_BIM="https://www.dropbox.com/scl/fo/3wyjvc06ch4agtvslfrk4/ADjjXhKolBgF8VwtGmEvw38/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.match1000G_GSAs_dietnet.bim?rlkey=bkanr0l2lvbqrd037j7kjeovy&e=1&dl=1"
-URL_FAM="https://www.dropbox.com/scl/fo/3wyjvc06ch4agtvslfrk4/AC0jarjSKKup55pbAvRWOJo/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.match1000G_GSAs_dietnet.fam?rlkey=bkanr0l2lvbqrd037j7kjeovy&e=1&dl=1"
+URL_BED="https://www.dropbox.com/scl/fo/wtegmicc9cxyd1ggwbpsf/AAGNKU3k4LYDLOnQPyWzj8c/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.commonMHI_CaG17k.miss10perc.maf0.05.LDpruned.woLowComplexity_noHLA.bed?rlkey=udwf9dw0d0mt8dk3s3lpyhaoq&dl=1"
+URL_BIM="https://www.dropbox.com/scl/fo/wtegmicc9cxyd1ggwbpsf/AAYw1FGAOy-Ipc5Cq4zQN3o/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.commonMHI_CaG17k.miss10perc.maf0.05.LDpruned.woLowComplexity_noHLA.bim?rlkey=udwf9dw0d0mt8dk3s3lpyhaoq&dl=1"
+URL_FAM="https://www.dropbox.com/scl/fo/wtegmicc9cxyd1ggwbpsf/AM-eWoWihBzq4DOZR41Vx24/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.commonMHI_CaG17k.miss10perc.maf0.05.LDpruned.woLowComplexity_noHLA.fam?rlkey=udwf9dw0d0mt8dk3s3lpyhaoq&dl=1"
 
 # Download the files into the new folder.
-wget -O data/HGDP+1KGP/genotypes/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.match1000G_GSAs_dietnet.bed "$URL_BED"
-wget -O data/HGDP+1KGP/genotypes/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.match1000G_GSAs_dietnet.bim "$URL_BIM"
-wget -O data/HGDP+1KGP/genotypes/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.match1000G_GSAs_dietnet.fam "$URL_FAM"
+wget -O data/HGDP+1KGP/genotypes/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.commonMHI_CaG17k.miss10perc.maf0.05.LDpruned.woLowComplexity_noHLA.bed "$URL_BED"
+wget -O data/HGDP+1KGP/genotypes/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.commonMHI_CaG17k.miss10perc.maf0.05.LDpruned.woLowComplexity_noHLA.bim "$URL_BIM"
+wget -O data/HGDP+1KGP/genotypes/gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.commonMHI_CaG17k.miss10perc.maf0.05.LDpruned.woLowComplexity_noHLA.fam "$URL_FAM"
 
 # Optional additional downloads:
 
