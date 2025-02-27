f613dbdd2d0eb7f45c5df3aba7e1bb41eb59bd67
nishuang83 <nishuang83@gmail.com>
2025-02-26 23:33:25 -0500

    add diffusion map method, module and config

---
+++ src/algorithms/diffusionmap.py
@@ -0,0 +1,52 @@
+
+import torch
+from torch import Tensor
+from typing import Optional, Union
+from .utils.diffusion_map import DiffusionMap
+
+from .dimensionality_reduction import DimensionalityReductionModule
+
+class DiffusionMapModule(DimensionalityReductionModule):
+    def __init__(
+        self,
+        n_components: int = 2,
+        random_state: Optional[int] = 42,
+        knn: Optional[int] = 5,
+        t: Union[int, str] = 15, # Can be an integer or 'auto'
+        decay: Optional[int] = 40,
+        gamma: Optional[float] = 1, 
+        n_pca: Optional[int] = None,
+        n_landmark: Optional[int] = 2000,
+        n_jobs: Optional[int] = -1,
+        verbose = False,
+        fit_fraction: float = 1.0,  # Fraction of data used for fitting
+    ):
+        super().__init__(n_components, random_state, knn, t, decay, gamma, n_pca, n_landmark, n_jobs, verbose)
+        self.fit_fraction = fit_fraction
+        self.model = DiffusionMap(n_components=self.n_components, 
+                           random_state=self.random_state,
+                           knn=self.knn,
+                           t=self.t,
+                           decay=self.decay,
+                           gamma=self.gamma,
+                           n_pca=self.n_pca,
+                           n_landmark=self.n_landmark,
+                           n_jobs=self.n_jobs,
+                           verbose=self.verbose)
+
+    def fit(self, x: Tensor) -> None:
+        """Fits DiffusionMap on a subset of data."""
+        x_np = x.detach().cpu().numpy()
+        n_samples = x_np.shape[0]
+        n_fit = max(1, int(self.fit_fraction * n_samples))  # Use only a fraction of the data
+        self.model.fit(x_np[:n_fit])
+        self._is_fitted = True
+
+    def transform(self, x: Tensor) -> Tensor:
+        """Transforms data using the fitted DiffusionMap model."""
+        if not self._is_fitted:
+            raise RuntimeError("DiffusionMap model is not fitted yet. Call `fit` first.")
+        
+        x_np = x.detach().cpu().numpy()
+        embedding = self.model.transform(x_np)
+        return torch.tensor(embedding, device=x.device, dtype=x.dtype)
