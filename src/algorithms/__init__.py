1e68dd5d7f37d503cd123277072730c356425b7f
nishuang83 <nishuang83@gmail.com>
2025-03-11 13:04:22 -0400

    update init for UMAP

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -2,8 +2,9 @@
 from hydra_zen import builds, store
 
 from .pca import PCAModule
-from .phate import PHATE
-from .tsne import tSNE
+from .phate import PHATEModule
+from .tsne import TSNEModule
+from .umap import UMAPModule
 
 algorithm_store = store(group="algorithm")
 
@@ -17,8 +18,8 @@ algorithm_store(pca_config, name="pca")
 
 ## review PHATE and TSNE after they're implemented, currently placeholders
 phate_config = builds(
-    PHATE,
-    n_components="${cfg.algorithm.n_components:50}",
+    PHATEModule,
+    n_components="${cfg.algorithm.n_components:2}",
     knn="${cfg.algorithm.knn:30}",
     gamma="${cfg.algorithm.gamma:0.5}",
     populate_full_signature=True
@@ -34,3 +35,14 @@ tsne_config = builds(
     populate_full_signature=True
 )
 algorithm_store(tsne_config, name="tsne")
+
+umap_config = builds(
+    UMAPModule,
+    n_components="${cfg.algorithm.n_components:2}",
+    random_state="${cfg.algorithm.random_state:42}",
+    n_neighbors="${cfg.algorithm.n_neighbors:15}",
+    n_epochs="${cfg.algorithm.n_epochs:500}",
+    learning_rate="${cfg.algorithm.learning_rate:1.0}",
+    populate_full_signature=True
+)
+algorithm_store(umap_config, name="umap")
