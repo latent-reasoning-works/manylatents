af1de05f3ed6a39d6f886d637baf4e40281088fb
Matthew Scicluna <sciclun4@narval2.narval.calcul.quebec>
2025-04-10 11:16:55 -0400

    updated admixture preservation generation code. Added admixture ratios for new genotype data. Updated download_data.sh with new link

--- scripts/clean_up_admixture.sh
+++ scripts/clean_up_admixture.sh
@@ -3,7 +3,5 @@
 
 # Dropbox direct download link
 root="data/HGDP+1KGP"
-plink_root="${root}/genotypes/no_intersection_less_stringent"
-plink_fname="gnomad.genomes.v3.1.2.hgdp_tgp.PASSfiltered.newIDs.onlySNPs.noDuplicatePos.noMiss5perc.maf0.01.LDpruned_500_50_0.05.woLowComplexity_noHLA"
 
-python scripts/clean_up_admixture.py --plink_prefix ${plink_root}/${plink_fname} --metadata_path ${root}/"gnomad_derived_metadata_with_filtered_sampleids.csv" --admix_file_root ${root}/"admixture"
+python scripts/clean_up_admixture.py --indices_file ${root}/"admixture/indices.txt" --metadata_path ${root}/"gnomad_derived_metadata_with_filtered_sampleids.csv" --admix_file_root ${root}/"admixture"
