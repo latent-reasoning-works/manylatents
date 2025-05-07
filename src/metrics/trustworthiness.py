c56ea9698e771ba34490cd55d42a291ff72f9e28
Matthew Scicluna <mattcscicluna@gmail.com>
2025-05-07 13:16:11 -0400

    Updated {geographic,admix,knn} preservation and trustworthiness to new metric signature (#69)
    * added configs for dlatree sweep
    
    * updated metrics to new signature
    
    * changed order of args to conform with protocol

--- src/metrics/trustworthiness.py
+++ src/metrics/trustworthiness.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 import numpy as np
 from sklearn.manifold import trustworthiness as sk_trustworthiness
-
+from src.algorithms.dimensionality_reduction import DimensionalityReductionModule
 
 def Trustworthiness(embeddings: np.ndarray, 
                     dataset: object, 
+                    module: Optional[DimensionalityReductionModule] = None,
                     n_neighbors: int = 25, 
                     metric: str = 'euclidean') -> float:
     """
