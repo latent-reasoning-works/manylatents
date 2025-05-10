a338dfd8531ab3356439f26bd905c35d4bc262b2
Shuang Ni <54051537+nishuang83@users.noreply.github.com>
2025-05-09 23:12:57 -0400

    Add embryoid dataset (#73)
    * add single_cell and EmbryoidBody PyTorch Dataset
    
    * add EmbryoidBody Lightning DataModule
    
    * add EmbryoidBody config
    
    * add eb sweep example
    
    * add precomputed sweep config
    
    * add scRNAseq download script
    
    * update URL
    
    merging, cleaning up later when we decide on a dataset API standard.

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -25,3 +25,31 @@ if [ ! -d "$DIR_HGDP_1KGP" ]; then
 else
     echo "Directory $DIR_HGDP_1KGP already exists, skipping extraction."
 fi
+
+
+# Downloads scRNAseq zip file from Dropbox
+
+# Dropbox direct download link
+URL_scRNAseq="https://www.dropbox.com/scl/fi/f512xl4b128q4t55jdsfc/scRNAseq.tar.gz?rlkey=d2rzidzm8asmvegkq7blo7che&st=xo8cxkt7&dl=1"
+
+# Local path for the zip file
+FILE_scRNAseq="data/scRNAseq.tar.gz"
+
+# Local extraction directory
+DIR_scRNAseq="data/scRNAseq"
+
+# Download the file if it doesn't already exist
+if [ ! -f "$FILE_scRNAseq" ]; then
+    echo "Downloading scRNAseq..."
+    wget -O "$FILE_scRNAseq" "$URL_scRNAseq"
+else
+    echo "File $FILE_scRNAseq already exists, skipping download."
+fi
+
+# Extract only if the directory doesn't exist
+if [ ! -d "$DIR_scRNAseq" ]; then
+    echo "Extracting scRNAseq.tar.gz..."
+    tar -xzvf "$FILE_scRNAseq" -C data/
+else
+    echo "Directory $DIR_scRNAseq already exists, skipping extraction."
+fi
