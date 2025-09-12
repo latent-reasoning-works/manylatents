c8f2980a773ed1f637575ee97ef0c9851e35ce17
Matt Scicluna <mattcscicluna@gmail.com>
2025-09-12 13:19:14 -0400

    changed dlatree cmap to be dict like the rest

--- manylatents/utils/mappings.py
+++ manylatents/utils/mappings.py
@@ -278,15 +278,15 @@ race_only_pca_colors.update(gray_mapping)
 ethnicity_only_pca_colors.update(gray_mapping)
 
 # Colorful palette for DLA tree branches (up to 10 branches)
-cmap_dla_tree = ListedColormap([
-    '#e41a1c',  # red
-    '#377eb8',  # blue  
-    '#4daf4a',  # green
-    '#984ea3',  # purple
-    '#ff7f00',  # orange
-    '#ffff33',  # yellow
-    '#a65628',  # brown
-    '#f781bf',  # pink
-    '#999999',  # gray
-    '#66c2a5'   # teal
-])
+cmap_dla_tree = {
+    1: '#e41a1c',  # red
+    2: '#377eb8',  # blue  
+    3: '#4daf4a',  # green
+    4: '#984ea3',  # purple
+    5: '#ff7f00',  # orange
+    6: '#ffff33',  # yellow
+    7: '#a65628',  # brown
+    8: '#f781bf',  # pink
+    9: '#999999',  # gray
+    10: '#66c2a5'   # teal
+}
